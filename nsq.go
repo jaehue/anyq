984815741eff5e4eaa5ef6a844e35a4765183a3a
jaehue <jaehue@jang.io>
2015-05-08 10:35:08 +0900

    queue abstracting

--- nsq.go
+++ nsq.go
@@ -3,19 +3,66 @@ package qlib
 import (
 	"github.com/bitly/go-nsq"
 	"log"
+	"reflect"
 )
 
-type Nsq struct {
-	Url string
+func init() {
+	Register("nsq", &Nsq{})
 }
 
-func NewNsq(url string) *Nsq {
-	q := &Nsq{url}
+type Nsq struct {
+	Url, Topic, Channel string
+}
+
+func (q *Nsq) Setup(url string, options ...func(interface{}) interface{}) {
+	for _, option := range options {
+		if v := option(q); v != nil {
+			if x, ok := v.(*Nsq); ok {
+				q = x
+			}
+		}
+	}
+	q.Url = url
+	log.Println("setup q: ", q)
+
+}
+
+func (q *Nsq) Setup2(options ...func(*Nsq)) {
+	for _, option := range options {
+		option(q)
+	}
+}
+
+func NewNsqWithOption(options ...func(*Nsq)) *Nsq {
+	q := &Nsq{}
+
+	for _, option := range options {
+		option(q)
+	}
+
 	return q
 }
 
-func (q *Nsq) BindRecvChan(topic, channel string, recvCh chan<- []byte) error {
-	c, err := nsq.NewConsumer(topic, channel, nsq.NewConfig())
+func NewNsq(options ...func(interface{}) interface{}) *Nsq {
+	q := Nsq{}
+
+	log.Println(reflect.TypeOf(q))
+	for _, option := range options {
+		if v := option(&q); v != nil {
+			if x, ok := v.(*Nsq); ok {
+				log.Println("set nsq")
+				q = *x
+			}
+		}
+	}
+
+	log.Println(q)
+	return &q
+}
+
+func (q *Nsq) BindRecvChan(recvCh chan<- []byte) error {
+	log.Println("recv ", q)
+	c, err := nsq.NewConsumer(q.Topic, q.Channel, nsq.NewConfig())
 	if err != nil {
 		return err
 	}
@@ -29,7 +76,7 @@ func (q *Nsq) BindRecvChan(topic, channel string, recvCh chan<- []byte) error {
 	return c.ConnectToNSQD(q.Url)
 }
 
-func (q *Nsq) BindSendChan(topic string, sendCh <-chan []byte) error {
+func (q *Nsq) BindSendChan(sendCh <-chan []byte) error {
 	p, err := nsq.NewProducer(q.Url, nsq.NewConfig())
 	if err != nil {
 		return err
@@ -42,7 +89,7 @@ func (q *Nsq) BindSendChan(topic string, sendCh <-chan []byte) error {
 	}()
 	go func() {
 		for m := range sendCh {
-			p.PublishAsync(topic, m, done)
+			p.PublishAsync(q.Topic, m, done)
 		}
 	}()
 
--- nsq_test.go
+++ nsq_test.go
@@ -2,13 +2,43 @@ package qlib
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
+func setupNsq(q interface{}) interface{} {
+	fmt.Println(reflect.TypeOf(q))
+	v, ok := q.(Nsq)
+	if ok {
+		fmt.Println("return")
+		return nil
+	}
+
+	nsq := v
+
+	nsq.Url = "192.168.81.43:4150"
+	nsq.Topic = "test"
+	nsq.Channel = "qlib_test"
+	fmt.Println("nsq", nsq)
+	return &nsq
+}
+
+func setupNsq2(q *Nsq) {
+	q.Url = "192.168.81.43:4150"
+	q.Topic = "test"
+	q.Channel = "qlib_test"
+}
+
 func TestConsume(t *testing.T) {
-	q := NewNsq("192.168.81.43:4150")
+	fmt.Println("start consume test")
+
+	q := NewNsqWithOption(setupNsq2)
+	// q := &Nsq{}
+	// q.Setup(setupNsq2)
+	//q := NewNsq(setupNsq)
+
 	recvCh := make(chan []byte, 100)
-	err := q.BindRecvChan("test", "qlib", recvCh)
+	err := q.BindRecvChan(recvCh)
 	if err != nil {
 		t.Error(err)
 	}
@@ -19,11 +49,12 @@ func TestConsume(t *testing.T) {
 }
 
 func BenchmarkProduce(b *testing.B) {
+	fmt.Println("start benchmark test")
 	b.StopTimer()
 
-	q := NewNsq("192.168.81.43:4150")
+	q := NewNsq(setupNsq)
 	sendCh := make(chan []byte, 100)
-	err := q.BindSendChan("test", sendCh)
+	err := q.BindSendChan(sendCh)
 	if err != nil {
 		b.Error(err)
 	}
