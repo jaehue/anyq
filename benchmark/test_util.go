2d08e8b218e788e32059a546c2db392f000d70cd
jaehue <jaehue@jang.io>
2015-05-11 15:38:47 +0900

    add pubsubBenchmark function

--- benchmark/test_util.go
+++ benchmark/test_util.go
@@ -6,6 +6,8 @@ import (
 	"testing"
 )
 
+var testMsg = `Go is an open source programming language that makes it easy to build simple, reliable, and efficient software.`
+
 func produceBenchmark(b *testing.B, q qlib.Queuer, args interface{}) {
 	sendCh := make(chan []byte, 100)
 	err := q.BindSendChan(sendCh, args)
@@ -13,11 +15,9 @@ func produceBenchmark(b *testing.B, q qlib.Queuer, args interface{}) {
 		b.Error(err)
 	}
 
-	msg := "test message"
-
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		body := fmt.Sprintf("[%d]%s", i, msg)
+		body := fmt.Sprintf("[%d]%s", i, testMsg)
 		sendCh <- []byte(body)
 	}
 }
@@ -33,4 +33,32 @@ func consumeBenchmark(b *testing.B, q qlib.Queuer, args interface{}) {
 	for i := 0; i < b.N; i++ {
 		<-recvCh
 	}
+}
+
+func pubsubBenchmark(b *testing.B, q qlib.Queuer, produceArgs interface{}, consumeArgs interface{}) {
+	recvCh := make(chan []byte, 100)
+	err := q.BindRecvChan(recvCh, consumeArgs)
+	if err != nil {
+		b.Error(err)
+	}
+
+	cnt := 0
+	go func() {
+		for _ = range recvCh {
+			cnt++
+		}
+	}()
+
+	sendCh := make(chan []byte, 100)
+	err = q.BindSendChan(sendCh, produceArgs)
+	if err != nil {
+		b.Error(err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		body := fmt.Sprintf("[%d]%s", i, testMsg)
+		sendCh <- []byte(body)
+	}
+	fmt.Println("receive count: ", cnt)
 }
