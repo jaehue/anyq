f45b19e5d60c1a3c526e5c7859665a08fa890258
jaehue <jaehue@jang.io>
2015-05-12 23:59:00 +0900

    refactoring interface - nsq

--- nsq.go
+++ nsq.go
@@ -11,80 +11,92 @@ func init() {
 }
 
 type Nsq struct {
-	url string
-
-	quits []chan struct{}
-	quit  chan struct{}
+	url     string
+	closers []closer
 }
 
-type NsqConsumeArgs struct {
+type NsqConsumerArgs struct {
 	Topic, Channel string
 }
-type NsqProduceArgs struct {
+type NsqProducerArgs struct {
 	Topic string
+}
+
+type nsqConsumer struct {
+	*nsq.Consumer
+	url string
+}
+
+type nsqProducer struct {
+	*nsq.Producer
+	topic string
 }
 
 func (q *Nsq) Setup(url string) error {
 	q.url = url
-	q.quit = make(chan struct{})
 	return nil
 }
 
-func (q *Nsq) Cleanup() error {
-	defer func() {
-		log.Printf("NSQ shutdown OK")
-		q.quit <- struct{}{}
-	}()
-
-	for _, quit := range q.quits {
-		quit <- struct{}{}
-	}
-
-	return nil
-}
-
-func (q *Nsq) Quit() <-chan struct{} {
-	return q.quit
-}
-
-func (q *Nsq) BindRecvChan(recvCh chan<- []byte, args interface{}) error {
-	consumeArgs, ok := args.(NsqConsumeArgs)
+func (q *Nsq) NewConsumer(v interface{}) (Consumer, error) {
+	args, ok := v.(NsqConsumerArgs)
 	if !ok {
-		return fmt.Errorf("invalid consume arguments(%v)", args)
+		return nil, fmt.Errorf("invalid consume arguments(%v)", v)
 	}
 
-	c, err := nsq.NewConsumer(consumeArgs.Topic, consumeArgs.Channel, nsq.NewConfig())
+	c, err := nsq.NewConsumer(args.Topic, args.Channel, nsq.NewConfig())
 	if err != nil {
-		return err
+		return nil, err
 	}
-
-	c.AddConcurrentHandlers(nsq.HandlerFunc(func(m *nsq.Message) error {
-		log.Println("receive message: ", string(m.Body))
-		recvCh <- m.Body
-		return nil
-	}), 1)
-
-	go func() {
-		quit := make(chan struct{})
-		q.quits = append(q.quits, quit)
-
-		<-quit
-		c.Stop()
-	}()
-
-	return c.ConnectToNSQD(q.url)
+	consumer := &nsqConsumer{c, q.url}
+	q.closers = append(q.closers, consumer)
+	return consumer, nil
 }
 
-func (q *Nsq) BindSendChan(sendCh <-chan []byte, v interface{}) error {
-	args, ok := v.(NsqProduceArgs)
+func (q *Nsq) NewProducer(v interface{}) (Producer, error) {
+	args, ok := v.(NsqProducerArgs)
 	if !ok {
-		return fmt.Errorf("invalid consume arguments(%T)", v)
+		return nil, fmt.Errorf("invalid consume arguments(%T)", v)
 	}
 
 	p, err := nsq.NewProducer(q.url, nsq.NewConfig())
 	if err != nil {
+		return nil, err
+	}
+
+	producer := &nsqProducer{p, args.Topic}
+	q.closers = append(q.closers, producer)
+	return producer, nil
+}
+
+func (q *Nsq) Close() error {
+	for _, c := range q.closers {
+		c.Close()
+	}
+
+	log.Printf("NSQ shutdown OK")
+
+	return nil
+}
+
+func (c *nsqConsumer) BindRecvChan(messages chan<- *Message) error {
+	c.AddConcurrentHandlers(nsq.HandlerFunc(func(m *nsq.Message) error {
+		log.Println("receive message: ", string(m.Body))
+		messages <- &Message{Body: m.Body, Origin: m}
+		return nil
+	}), 1)
+
+	if err := c.ConnectToNSQD(c.url); err != nil {
 		return err
 	}
+	return nil
+}
+
+func (c *nsqConsumer) Close() error {
+	c.Stop()
+	return nil
+}
+
+func (p *nsqProducer) BindSendChan(messages <-chan []byte) error {
 	done := make(chan *nsq.ProducerTransaction, 1000000)
 	go func() {
 		for {
@@ -92,19 +104,16 @@ func (q *Nsq) BindSendChan(sendCh <-chan []byte, v interface{}) error {
 		}
 	}()
 	go func() {
-		for body := range sendCh {
+		for body := range messages {
 			log.Println("send message: ", string(body))
-			p.PublishAsync(args.Topic, body, done)
+			p.PublishAsync(p.topic, body, done)
 		}
 	}()
 
-	go func() {
-		quit := make(chan struct{})
-		q.quits = append(q.quits, quit)
+	return nil
+}
 
-		<-quit
-		p.Stop()
-	}()
-
+func (p *nsqProducer) Close() error {
+	p.Stop()
 	return nil
 }
