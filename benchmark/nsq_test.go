e873cf24d17e5b83417f7194e2a57ecd9a307598
jaehue <jaehue@jang.io>
2015-05-13 10:21:42 +0900

    edit queue url(local queue)

--- benchmark/nsq_test.go
+++ benchmark/nsq_test.go
@@ -10,7 +10,7 @@ import (
 func BenchmarkNsqProduce(b *testing.B) {
 	log.SetOutput(ioutil.Discard)
 
-	q, err := anyq.New("nsq", "192.168.81.43:4150")
+	q, err := anyq.New("nsq", "127.0.0.1:4150")
 	q.SetLogger(log.New(ioutil.Discard, "", log.LstdFlags), anyq.LogLevelInfo)
 	if err != nil {
 		b.Error(err)
@@ -21,7 +21,7 @@ func BenchmarkNsqProduce(b *testing.B) {
 func BenchmarkNsqConsume(b *testing.B) {
 	log.SetOutput(ioutil.Discard)
 
-	q, err := anyq.New("nsq", "192.168.81.43:4150")
+	q, err := anyq.New("nsq", "127.0.0.1:4150")
 	q.SetLogger(log.New(ioutil.Discard, "", log.LstdFlags), anyq.LogLevelInfo)
 	if err != nil {
 		b.Error(err)
